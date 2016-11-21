fb3ccd5b4708b76858a55b18265531c516033773
madsonwar <madsonwar21@gmail.com>
2016-11-21 16:30:34 -0300

    Create SitedeVendas.h

---
+++ SitedeVendas.h
@@ -0,0 +1,13 @@
+#ifndef SITEDEVENDAS_H
+#define SITEDEVENDAS_H
+#include <string>
+
+class SitedeVendas{
+  public:
+          SitedeVendas();
+          void printfnomedoSite();
+          void printfEndereco();
+  private:
+          string nomedoSite;
+          string enderecodoSite;
+{
