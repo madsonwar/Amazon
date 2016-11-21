426d9d28c039e2a9278fcd88f3eb581bd533fa9e
madsonwar <madsonwar21@gmail.com>
2016-11-21 16:37:52 -0300

    Create SitedeVendas.cpp

---
+++ SitedeVendas.cpp
@@ -0,0 +1,25 @@
+#include "SitedeVendas.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+SitedeVendas::SitedeVendas()
+{
+
+}
+
+SitedeVendas::~SitedeVendas()
+{
+}
+
+
+void SitedeVendas::nomedoSite()
+{
+    cout<<"O nome do site e:" << nomedoSite << "\n";
+    
+}
+
+void SitedeVendas::enderecodoSite()
+{
+     cout<<"O endereco do site e:" << enderecodoSite << "\n";
+}
